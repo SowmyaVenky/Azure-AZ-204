6860e074d1cc839e368a1bca11ea81e3332c2b9f
Venkata Jagannath <venky_jagannath@hotmail.com>
2022-11-21 11:01:20 -0600

    added code to push temp file to API

--- microsoft-labs/Lab01/webapp/src/main/java/com/gssystems/web/controller/UploadController.java
+++ microsoft-labs/Lab01/webapp/src/main/java/com/gssystems/web/controller/UploadController.java
@@ -5,15 +5,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadController {
+	//API to push file to storage
+	private static String FILE_UPLOAD_API_ENDPOINT = "http://localhost:9090/uploadFile/";
 	@GetMapping("/uploadimage")
 	public String displayUploadForm() {
 		return "imageupload/index";
@@ -23,8 +33,30 @@ public class UploadController {
 	public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
 		StringBuilder fileNames = new StringBuilder();
 		fileNames.append(file.getOriginalFilename());
-		System.out.println("Image uploaded is: " + file.getName() + " is of size: " + file.getBytes().length);
+		System.out.println("Image uploaded is: " + file.getOriginalFilename() + " is of size: " + file.getBytes().length);
+		uploadSingleFile(file);
 		model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
 		return "imageupload/index";
 	}
+
+	private static void uploadSingleFile(MultipartFile file) throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		
+		String fileToUpload = file.getOriginalFilename();
+	    String ext = fileToUpload.substring(fileToUpload.lastIndexOf(".") + 1);
+	    String filenamenoext = fileToUpload.substring(0, fileToUpload.lastIndexOf("."));
+	    Path tempFile = Files.createTempFile(filenamenoext, "." + ext);
+	    Files.write(tempFile, file.getBytes());
+		
+		body.add("file", new FileSystemResource(tempFile.toFile()));
+
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.postForEntity(FILE_UPLOAD_API_ENDPOINT, requestEntity, String.class);
+		System.out.println("Response code: " + response.getStatusCode());
+	}
+
 }
