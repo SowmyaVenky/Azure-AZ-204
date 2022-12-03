0ebbb9f79bb96375e116ca285b5f682a9a0a8737
Venkata Jagannath <venky_jagannath@hotmail.com>
2022-12-03 15:50:54 -0600

    added spring boot app

--- microsoft-labs/Lab05/vm-install.sh
+++ microsoft-labs/Lab05/vm-install.sh
@@ -23,32 +23,6 @@ sudo docker run --name=venkymysql -e MYSQL_ROOT_PASSWORD=Ganesh20022002 -d mysql
 sudo docker exec -it venkymysql bash
 
 ## Sample springboot app with mysql 
-git clone https://github.com/spring-guides/gs-accessing-data-mysql.git
-cd /home/venky/gs-accessing-data-mysql
-rm -rf test initial
-cd complete
-
 sudo apt install wget
-wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz
-tar xzf OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz
-sudo mv jdk-17.0.1+12 /opt/
-
-export JAVA_HOME=/opt/jdk-17.0.1+12
-export PATH=$JAVA_HOME/bin:$PATH
-
 sudo apt install -y default-jdk maven
-
-#jdk 17
-wget https://download.java.net/openjdk/jdk17/ri/openjdk-17+35_linux-x64_bin.tar.gz
-tar xvf openjdk-17+35_linux-x64_bin.tar.gz
-sudo mv jdk-17*/ /opt/jdk17
-
-sudo tee /etc/profile.d/jdk.sh <<EOF
-export JAVA_HOME=/opt/jdk17
-export PATH=\$JAVA_HOME/bin:\$PATH
-EOF
-
-source /etc/profile.d/jdk.sh
-
-
 mvn clean package
